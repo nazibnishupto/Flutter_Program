402d35506e37873d31ae26fd1118000558ceb85d
nazibnishupto <nishuptonazib@gmail.com>
2025-02-23 16:36:34 +0600

    ofc 23 feb

--- Module/lib/Home.dart
+++ Module/lib/Home.dart
@@ -3,84 +3,40 @@ import 'package:flutter/material.dart';
 
 import 'UserInfo.dart';
 
-class Home extends StatefulWidget {
-  const Home({super.key});
-
-  @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  TextEditingController _taskController = TextEditingController();
-
-  List<String> _task = [];
-
-  @override
-  void initState() {
-    super.initState();
-
-  }
-
-  _addTask(){
-    if(_taskController.text.isNotEmpty){
-      setState(() {
-        _task.add(_taskController.text);
-        _taskController.clear();
-      });
-    }
-  }
-
-  _removeTask(int index){
-    setState(() {
-      _task.removeAt(index);
-    });
-  }
-
-  _removeALL(){
-    setState(() {
-      _task.clear();
-    });
-  }
-
+class Home extends StatelessWidget {
+  Home({super.key});
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
-        title: Text("TO DO LIST", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white)),
+        title: Text("MediaQuery",
+            style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.white)),
+        backgroundColor: Colors.red,
         centerTitle: false,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _taskController,
-              decoration: InputDecoration(
-                hintText: "Enter your test",
-                border: OutlineInputBorder(),
-                suffixIcon: IconButton(onPressed: _addTask, icon: Icon(Icons.add)),
-              ),
+      body: Center(
+        child: Container(
+          height: screenHeight,
+          width: screenHeight,
+          color: Colors.blueGrey,
+          child: Center(
+            child: Text(
+              "Responsive Design",
+              style: TextStyle(
+                  fontSize: 50,
+                  fontWeight:
+                  FontWeight.bold,
+                  color: Colors.white),
             ),
-            SizedBox(height: 20,),
-
-            Expanded(
-              child: ListView.builder(
-                  itemCount: _task.length,
-                  itemBuilder:(context,index){
-                    return Card(
-                      child: ListTile(
-                        title: Text(_task[index]),
-                        trailing: IconButton(onPressed: () => _removeTask(index), icon: Icon(Icons.delete), color: Colors.red,),
-                      ),
-                    );
-                  }
-              ),
-            )
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(onPressed: _removeALL, child: Icon(Icons.delete_sweep),),
     );
   }
 }
