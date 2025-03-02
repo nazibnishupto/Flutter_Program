a8a0bb881c8d0dfef0c58f24a163622e37300192
nazibnishupto <nishuptonazib@gmail.com>
2025-03-02 11:01:14 +0600

    ofc 02 mar

--- Module/lib/Home.dart
+++ Module/lib/Home.dart
@@ -13,6 +13,22 @@ class ImageExample extends StatelessWidget {
       appBar: AppBar(
         title: Text("Image"),
       ),
+      body: Column(
+        children: [
+          Center(
+            child: Image.network(
+                height: 200,
+                width: 200,
+                "https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
+          ),
+          Center(
+            child: Image.asset("ASSET/uncrop_hero_bdf08a8ca6.jpg",
+                width: 200,
+                height: 200,
+                fit: BoxFit.fitWidth),
+          )
+        ],
+      ),
     );
   }
 }
