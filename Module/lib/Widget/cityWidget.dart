0ec9c31969d41bb85ad8f6f8ee529e96115ce722
Nazib_Laptop <nishuptonazib@gmail.com>
2025-03-04 19:58:54 +0600

    Home_04_Mar

--- Module/lib/Widget/cityWidget.dart
+++ Module/lib/Widget/cityWidget.dart
@@ -11,7 +11,7 @@ class cityWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 40.h,
+      height: 50.h,
       width: 50.w,
       decoration: BoxDecoration(
         color: Colors.white,
