1b7b95fc8eafe51d8e0a9eec30b304efffc5dbbe
Nazib_Laptop <nishuptonazib@gmail.com>
2025-03-05 23:11:17 +0600

    assignment submission

---
+++ Assignment/lib/data/Models/ProductController.dart
@@ -0,0 +1,72 @@
+import 'dart:convert';
+
+import 'package:assignment/data/Models/product.dart';
+import 'package:assignment/data/utils/urls.dart';
+
+import 'package:http/http.dart' as http;
+
+
+class ProductController {
+  List<Data> products = [];
+
+  Future<void> fetchProducts() async {
+    final response = await http.get(Uri.parse(Urls.readProduct));
+
+    print(response.statusCode);
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      productModel model = productModel.fromJson(data);
+      products = model.data ?? [];
+    }
+  }
+
+
+  Future<void> createProduct(String name,String img,int qty,int price,int totalPrice) async {
+    final response = await http.post(Uri.parse(Urls.createProduct),
+        headers: {'Content-Type' : 'application/json'},
+        body: jsonEncode({
+          "ProductName": name,
+          "ProductCode": DateTime.now().microsecondsSinceEpoch,
+          "Img": img,
+          "Qty": qty,
+          "UnitPrice": price,
+          "TotalPrice": totalPrice
+        })
+    );
+
+    print(response);
+    if (response.statusCode == 201) {
+      fetchProducts();
+    }
+  }
+
+  Future<void> UpdateProduct(String id,String name,String img,int qty,int price,int totalPrice) async {
+    final response = await http.post(Uri.parse(Urls.updateProduct(id)),
+        headers: {'Content-Type' : 'application/json'},
+        body: jsonEncode({
+          "ProductName": name,
+          "ProductCode": DateTime.now().microsecondsSinceEpoch,
+          "Img": img,
+          "Qty": qty,
+          "UnitPrice": price,
+          "TotalPrice": totalPrice
+        })
+    );
+
+    print(response);
+    if (response.statusCode == 201) {
+      fetchProducts();
+    }
+  }
+
+  Future<bool> deleteProducts(String id) async {
+    final response = await http.get(Uri.parse(Urls.deleteProduct(id)));
+
+    print(response.statusCode);
+    if (response.statusCode == 200) {
+      return true;
+    }else{
+      return false;
+    }
+  }
+}
---
+++ Assignment/lib/myApp.dart
@@ -0,0 +1,36 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+import 'package:assignment/presentation/ui/screens/Home.dart';
+
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Sizer(
+        builder: (context, orientation, screenType) {
+          return MaterialApp(
+            useInheritedMediaQuery: true,
+            locale: DevicePreview.locale(context),
+            builder: DevicePreview.appBuilder,
+            theme: ThemeData(
+                primaryColor: Colors.red,
+                appBarTheme: AppBarTheme(
+                  color: Colors.blueGrey,
+                  centerTitle: true,
+                ),
+                scaffoldBackgroundColor: Colors.white,
+                elevatedButtonTheme: ElevatedButtonThemeData(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.deepPurpleAccent,
+                        foregroundColor: Colors.white))),
+            title: 'Ostad Flutter app',
+
+            home: CRUDexample(),
+          );
+        }
+    );
+  }
+}
