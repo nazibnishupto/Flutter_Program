d481b869b2b7f0ca61c399de2c18e8a04c4077a5
nazibnishupto <nishuptonazib@gmail.com>
2025-05-24 09:05:41 +0600

    ofc 24 May

--- Task_management_Project/lib/app.dart
+++ Task_management_Project/lib/app.dart
@@ -1,62 +1,13 @@
-import 'package:get/get.dart';
 import 'package:flutter/material.dart';
-import 'controller_binder.dart';
-import 'ui/screens/splash_screen.dart';
+import 'home_screen.dart';
 
-class TaskManagementApp extends StatefulWidget {
-  const TaskManagementApp({super.key});
+class LiveScoreApp extends StatelessWidget {
+  const LiveScoreApp({super.key});
 
-  static final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
-
-  @override
-  State<TaskManagementApp> createState() => _TaskManagementAppState();
-}
-
-class _TaskManagementAppState extends State<TaskManagementApp> {
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      navigatorKey: TaskManagementApp.navigatorKey,
-      theme: ThemeData(
-        colorSchemeSeed: Colors.green,
-        inputDecorationTheme: InputDecorationTheme(
-          hintStyle: TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.w400,
-            color: Colors.grey,
-          ),
-          fillColor: Colors.white,
-          filled: true,
-          contentPadding: EdgeInsets.symmetric(
-            horizontal: 16,
-          ),
-          border: _getZeroBorder(),
-          enabledBorder: _getZeroBorder(),
-          errorBorder: _getZeroBorder(),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-              fixedSize: Size(double.maxFinite, 40),
-              backgroundColor: Colors.green,
-              foregroundColor: Colors.white,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8),
-              )
-          ),
-        ),
-        textTheme: TextTheme(
-          titleLarge: const TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
-        )
-      ),
-      home: const SplashScreen(),
-      initialBinding: ControllerBinder(),
+    return const MaterialApp(
+      home: HomeScreen(),
     );
   }
-
-  OutlineInputBorder _getZeroBorder(){
-    return const OutlineInputBorder(
-      borderSide: BorderSide.none,
-    );
-  }
-
 }
--- Task_management_Project/lib/main.dart
+++ Task_management_Project/lib/main.dart
@@ -1,8 +1,13 @@
-import 'package:assignment/app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'app.dart';
 
-void main() {
-  runApp(
-    const TaskManagementApp(),
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
+  runApp(const LiveScoreApp());
 }
